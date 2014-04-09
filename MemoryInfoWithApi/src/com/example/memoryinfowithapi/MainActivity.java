953523129d4e0841276779ab26702b286e2ffda7
Eric Chen <eric.chen.zhuhai@software.dell.com>
2014-04-09 16:55:41 +0800

    update main layout of memory info projects

--- MemoryInfoWithApi/src/com/example/memoryinfowithapi/MainActivity.java
+++ MemoryInfoWithApi/src/com/example/memoryinfowithapi/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends Activity {
 @Override
 protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_main);
+	setContentView(com.example.testapplibrary.R.layout.memory_info_main);
 	initExecuteTask();
 	
 }
